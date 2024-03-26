liquity/bold

Apply aggregate interest when adjusting debt and/or coll

contracts/src/Interfaces/ITroveManager.sol
@@ -106,12 +106,9 @@ interface ITroveManager is ILiquityBase {
 
     function decreaseTroveDebt(address _borrower, uint _collDecrease) external returns (uint); 
 
-    function updateTroveDebtAndInterest(
-        address _borrower, 
-        uint256 _oldWeightedRecordedTroveDebt, 
-        uint256 _entireTroveDebt, 
-        uint256 _newAnnualInterestRate
-    ) external;
+    function updateTroveDebtAndInterest(address _borrower, uint256 _entireTroveDebt, uint256 _newAnnualInterestRate) external;
+
+    function updateTroveDebt(address _borrower, uint256 _entireTroveDebt) external;
 
     function getTCR(uint _price) external view returns (uint);
 

contracts/src/test/interestRateBasic.t.sol
@@ -24,7 +24,7 @@ contract InterestRateBasic is DevTestSetup {
         assertEq(troveManager.getTroveAnnualInterestRate(D), 1e18);
     }
 
-    function testOpenTroveSetsTroveLastDebtUpdateTime() public {
+    function testOpenTroveSetsTroveLastDebtUpdateTimeToNow() public {
         priceFeed.setPrice(2000e18);
         assertEq(troveManager.getTroveLastDebtUpdateTime(A), 0);
         assertEq(troveManager.getTroveLastDebtUpdateTime(B), 0);
@@ -223,4 +223,20 @@ contract InterestRateBasic is DevTestSetup {
         assertEq(sortedTroves.getNext(E), D);
         assertEq(sortedTroves.getPrev(E), ZERO_ADDRESS); // head
     }
+
+    // --- withdrawBold ---
+
+    function testWithdrawBoldSetsTroveLastDebtUpdateTimeToNow() public {
+        priceFeed.setPrice(2000e18);
+        openTroveNoHints100pctMaxFee(A,  3 ether, 2000e18,  0);
+
+        vm.warp(block.timestamp + 1 days);
+
+        assertLt(troveManager.getTroveLastDebtUpdateTime(A), block.timestamp);
+
+        // // A draws more debt
+        withdrawBold100pctMaxFee(A, 500e18);
+        assertEq(troveManager.getTroveLastDebtUpdateTime(A), block.timestamp);   
+    }
 }
+   

richardo-hohmann/bold_solidity

contracts/src/Interfaces/ITroveManager.sol
@@ -109,12 +109,9 @@ interface ITroveManager is IERC721, ILiquityBase {
 
     function changeAnnualInterestRate(uint256 _troveId, uint256 _newAnnualInterestRate) external;
 
-    function updateTroveDebtAndInterest(
-        uint256 _troveId,
-        uint256 _oldWeightedRecordedTroveDebt,
-        uint256 _entireTroveDebt,
-        uint256 _newAnnualInterestRate
-    ) external;
+    function updateTroveDebtAndInterest(uint256 _troveId, uint256 _entireTroveDebt, uint256 _newAnnualInterestRate) external;
+
+    function updateTroveDebt(uint256 _troveId, uint256 _entireTroveDebt) external;
 
     function setAddManager(address _sender, uint256 _troveId, address _manager) external;
     function setRemoveManager(address _sender, uint256 _troveId, address _manager) external;

contracts/src/test/interestRateBasic.t.sol
@@ -21,7 +21,7 @@ contract InterestRateBasic is DevTestSetup {
         assertEq(troveManager.getTroveAnnualInterestRate(D_Id), 1e18);
     }
 
-    function testOpenTroveSetsTroveLastDebtUpdateTime() public {
+    function testOpenTroveSetsTroveLastDebtUpdateTimeToNow() public {
         priceFeed.setPrice(2000e18);
         assertEq(troveManager.getTroveLastDebtUpdateTime(A), 0);
         assertEq(troveManager.getTroveLastDebtUpdateTime(B), 0);
@@ -219,5 +219,20 @@ contract InterestRateBasic is DevTestSetup {
         // Check E's neighbors unchanged
         assertEq(sortedTroves.getNext(E_Id), D_Id);
         assertEq(sortedTroves.getPrev(E_Id), 0); // head
+    }
+
+    // --- withdrawBold ---
+
+    function testWithdrawBoldSetsTroveLastDebtUpdateTimeToNow() public {
+        priceFeed.setPrice(2000e18);
+        uint256 ATroveId = openTroveNoHints100pctMaxFee(A,  3 ether, 2000e18,  0);
+
+        vm.warp(block.timestamp + 1 days);
+
+        assertLt(troveManager.getTroveLastDebtUpdateTime(ATroveId), block.timestamp);
+
+        // // A draws more debt
+        withdrawBold100pctMaxFee(ATroveId, 500e18);
+        assertEq(troveManager.getTroveLastDebtUpdateTime(ATroveId), block.timestamp);
     }
 }